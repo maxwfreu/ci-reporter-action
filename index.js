d2c61af35f2c3d493e2c674b3a13c80ae3ed2038
maxwfreu <mfreundlich1@gmail.com>
2021-12-24 14:43:06 -0500

    wrap in async func

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 
-  const response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
-    owner_repo: GITHUB_REPOSITORY,
-    run_id: GITHUB_RUN_ID,
+// Wrap an asynchronous function call
+const result = await core.group('Do something async', async () => {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+      
+        const response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
+          owner_repo: GITHUB_REPOSITORY,
+          run_id: GITHUB_RUN_ID,
+        })
+        return response;
+      } catch (error) {
+        core.setFailed(error.message);
+      }
   })
-  console.log(`The event payload: ${response}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+
+console.log(`The event payload: ${result}`);
